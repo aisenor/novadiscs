9f5444248d64924f072c5b42b281a60c94e4f779
Amanda Isenor <aisenor@resulta.com>
2022-02-25 16:01:24 -0400

    adding site content

---
+++ app/components/sendmail.php
@@ -0,0 +1,7 @@
+$phpmailer = new PHPMailer();
+$phpmailer->isSMTP();
+$phpmailer->Host = 'smtp.mailtrap.io';
+$phpmailer->SMTPAuth = true;
+$phpmailer->Port = 2525;
+$phpmailer->Username = '27082224839da8';
+$phpmailer->Password = 'e39c5acd642769';
