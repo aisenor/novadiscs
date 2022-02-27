68eb16902f2127e941dd5348507547e97e6d1f18
Amanda Isenor <aisenor@resulta.com>
2022-02-27 14:54:10 -0400

    Centered elements and added right scroll icon

--- app/components/sendmail.php
+++
@@ -1,7 +0,0 @@
-$phpmailer = new PHPMailer();
-$phpmailer->isSMTP();
-$phpmailer->Host = 'smtp.mailtrap.io';
-$phpmailer->SMTPAuth = true;
-$phpmailer->Port = 2525;
-$phpmailer->Username = '27082224839da8';
-$phpmailer->Password = 'e39c5acd642769';
